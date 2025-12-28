f1783d7fd4a6557ab7fac6cb66fff13f2abac95d
bhaleraom737@gmail.com <bhaleraom737@gmail.com>
2025-12-28 10:30:07 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectBD from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path:'./env'
 })
 
 connectBD()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server started at port : ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MongoBD connection failed", err)
+})
